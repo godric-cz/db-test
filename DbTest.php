d9492ec7d09c9f87d89feb8e9eb020ee116f4b2a
Godric <godric@korh.cz>
2018-02-18 16:51:50 +0100

    initial commit

---
+++ DbTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Godric\DbTest;
+
+foreach (glob(__DIR__ . '/*.php') as $f) require_once $f; // TODO
+
+class DbTest extends \PHPUnit\Framework\TestCase {
+
+    private static $connection;
+
+    static function setConnection(DbWrapper $connection) {
+        self::$connection = $connection;
+    }
+
+    function setUp() {
+        self::$connection->begin();
+    }
+
+    static function setUpBeforeClass() {
+        self::$connection->begin();
+
+        if(isset(static::$initData)) {
+            $dataset = new Dataset;
+            $dataset->addCsv(static::$initData);
+            self::$connection->import($dataset);
+        }
+    }
+
+    function tearDown() {
+        self::$connection->rollback();
+    }
+
+    static function tearDownAfterClass() {
+        self::$connection->rollback();
+    }
+
+}
